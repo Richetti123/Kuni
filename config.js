2eb663e5ef1ddf824d599ba5fc952c571f42f77c
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-06-07 02:23:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['51966254634', 'Richetti', true],
+  ['56928561853', 'Richetti', true],
   ['56941827124', 'Geripium', true],
-  ['18093040329', 'Natham', true],
+  ['5492216764542', 'KuniBot', true],
   ['51966254634', 'Richetti', true],
   ['51966254634', 'Richetti', true],
   ['51966254634', 'Richetti', true],
@@ -40,12 +40,12 @@ global.owner = [
 global.suittag = ['51966254634'];
 global.prems = ['51966254634'];
 
-global.packname = 'NathamBot';
-global.author = 'NathamBot';
-global.wm = 'NathamBot';
-global.titulowm = 'NathamBot';
-global.titulowm2 = `NathamBot`
-global.igfg = 'NathamBot';
+global.packname = 'KuniBot';
+global.author = 'KuniBot';
+global.wm = 'KuniBot';
+global.titulowm = 'KuniBot';
+global.titulowm2 = `KuniBot`
+global.igfg = 'KuniBot';
 global.wait = '*[ ⏳ ] Cargando...*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
@@ -70,11 +70,11 @@ global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\nNathamBot`;
-global.gt = 'NathamBot';
-global.mysticbot = 'NathamBot';
-global.md = 'https://www.instagram.com/natham.lara';
-global.mysticbot = 'https://www.instagram.com/natham.lara';
+global.wm2 = `${dia} ${fecha}\nKuniBot`;
+global.gt = 'KuniBot';
+global.mysticbot = 'KuniBot';
+global.md = 'https://www.instagram.com/kun10.ff';
+global.mysticbot = 'https://www.instagram.com/kun10.ff';
 global.waitt = '*[ ⏳ ] Cargando...*';
 global.waittt = '*[ ⏳ ] Cargando...*';
 global.waitttt = '*[ ⏳ ] Cargando...*';
